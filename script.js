82e838653cafd9df33f8a22dbf2fd0002c02901e
F飞行 <110045310@qq.com>
2025-05-24 10:19:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -63,11 +63,7 @@ function showTip(msg) {
 }
 // 不支持Web Share API时的备用分享方案
 
-function fallbackShare() {
-    navigator.clipboard.writeText('我发现了一个宝藏网站--趣加应用，分享给你：jqyy.store（在浏览器打开）');
-    alert('内容已复制到剪贴板！');
-}
-/*
+
 function fallbackShare() {
     navigator.clipboard.writeText('我发现了一个宝藏网站--趣加应用，分享给你：jqyy.store（在浏览器打开）')
     .then(() => {
@@ -77,7 +73,7 @@ function fallbackShare() {
         showTip('复制失败，请手动复制。');
     });
 }
-*/
+
 
 
 
