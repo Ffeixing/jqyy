4d4b796ea9c36a5422950ebf0e04ef2ec1012643
F飞行 <110045310@qq.com>
2025-05-25 13:39:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function showTip(msg) {
 }
 // 不支持Web Share API时的备用分享方案
 
-
+/*
 function fallbackShare() {
     navigator.clipboard.writeText('我发现了一个宝藏网站--趣加应用，分享给你：jqyy.store（在浏览器打开）')
     .then(() => {
@@ -73,7 +73,11 @@ function fallbackShare() {
         showTip('复制失败，请手动复制。');
     });
 }
-
+*/
+function fallbackShare() {
+    navigator.clipboard.writeText('我发现了一个宝藏网站--趣加应用，分享给你：jqyy.store（在浏览器打开）')
+    showTip('复制成功，去粘贴吧');
+}
 
 
 
